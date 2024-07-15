99f65000a9571d2ea9b2530c18de2fa540145388
Yashh Jaggi <yashhjaggi1998@gmail.com>
2024-07-14 22:42:32 -0700

    Adding Custom types

---
+++ types/AnnualOverviewData.ts
@@ -0,0 +1,10 @@
+export type AnnualOverviewData = {
+    percentReturn: number;
+    absoluteReturn: number;
+    absoluteReturnUSD: number;
+    amountInvested: number;
+    amountInvestedUSD: number;
+    currentInvestmentValue: number;
+    currentInvestmentValueUSD: number;
+    numberOfHoldings: number;
+}
