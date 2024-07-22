2dd6b3e01c19ec9522bb8f0adc56349fb049857b
Yashh Jaggi <yashhjaggi1998@gmail.com>
2024-07-21 19:32:19 -0700

    Componentizing portfolio page

---
+++ components/custom/tabs/ActiveHoldingsTab.tsx
@@ -0,0 +1,104 @@
+import { 
+    Avatar, 
+    AvatarImage,
+    AvatarFallback, 
+} from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { 
+    Table, 
+    TableCaption, 
+    TableHeader,
+    TableRow,
+    TableHead,
+    TableBody,
+    TableCell, 
+} from "@/components/ui/table";
+import { TabsContent } from "@/components/ui/tabs";
+import { FinancialYear } from "@/types/FinancialYear";
+
+interface ActiveHoldingsTabProps {
+    activeHoldings: any;
+    selectedFinancialYear: FinancialYear;
+}
+
+export function ActiveHoldingsTab(props: ActiveHoldingsTabProps) {
+
+    const { activeHoldings, selectedFinancialYear } = props;
+
+    return (
+        <TabsContent value="active_holdings" className="grid grid-cols-6 gap-4 mt-6">
+                            
+            <div className="col-span-6">
+                <Table className="text-center">
+                    {selectedFinancialYear && 
+                        <TableCaption className="font-bold text-sm mt-5">
+                            List of Active holdings for {selectedFinancialYear.year}
+                        </TableCaption>
+                    }
+                    <TableHeader className="border-b-2">
+                        <TableRow className="text-md h-12">
+                            <TableHead className="text-start"></TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>%PnL</TableHead>
+                            <TableHead>%Invested</TableHead>
+                            <TableHead>Absolute PnL (₹)</TableHead>
+                            <TableHead>Invested (₹)</TableHead>
+                            <TableHead>Cmp</TableHead>
+                            <TableHead>Buy Price</TableHead>
+                            <TableHead>Qty</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {activeHoldings.map((holding: any) => (
+                            <TableRow 
+                                key={holding._id} 
+                                className={`border-0 font-bold text-md font-bold hover:bg-slate-200 h-14`}
+                            >
+                                <TableCell className="items-center">
+                                    <div className="flex items-center space-x-4">
+                                        <Avatar>
+                                            <AvatarImage src={holding.logo_url} alt={holding.ticker} />
+                                            <AvatarFallback>CN</AvatarFallback>
+                                        </Avatar>
+                                        <span>
+                                            {holding.ticker}
+                                        </span>
+                                    </div>
+                                </TableCell>
+                                <TableCell>
+                                    <Badge 
+                                        className={`font-bold ${holding.percentPnL >= 25 ? "hover:bg-badgesuccess bg-badgesuccess text-green-900" : holding.percentPnL < -10 ? "hover:bg-badgewarning bg-badgewarning text-red-900" : "hover:bg-badgeinfo bg-badgeinfo text-blue-900"} `}>
+                                        {
+                                            holding.percentPnL >= 25 ? "STRONG" : 
+                                            holding.percentPnL < -10 ? "WEAK" : 
+                                            "MODERATE"
+                                        }
+                                    </Badge>
+                                </TableCell>
+                                <TableCell
+                                    className={`font-bold ${holding.percentPnL >= 25 ? "text-green-700" : holding.percentPnL < -10 ? "text-red-800" : "text-blue-900"} `}
+                                >
+                                    {holding.percentPnL}
+                                </TableCell>
+                                <TableCell>{30}</TableCell>
+                                <TableCell>
+                                    ₹ {holding.pnl}
+                                </TableCell>
+                                <TableCell>
+                                    ₹ {holding.quantity * holding.buy_price}
+                                </TableCell>
+                                <TableCell>
+                                    ₹ {holding.cmp}
+                                </TableCell>
+                                <TableCell>
+                                    ₹ {holding.buy_price}
+                                </TableCell>
+                                <TableCell>{holding.quantity}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
+        </TabsContent>
+    )
+}
--- components/custom/tabs/OverviewTab.tsx
+++ components/custom/tabs/OverviewTab.tsx
@@ -4,6 +4,8 @@ import { TabsContent } from "@/components/ui/tabs";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 import theme from "@/utils/theme";
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
+import { Badge } from "@/components/ui/badge";
 
 interface OverviewTabProps {
     overview_data: AnnualOverviewData;
@@ -14,9 +16,11 @@ export function OverviewTab(props: OverviewTabProps) {
     const { overview_data } = props;
 
     return (
-        <TabsContent value="overview">
+        <TabsContent value="overview" className="mt-8">
 
-            <div className="grid md:grid-cols-5 gap-4">
+            <h2 className="text-2xl font-bold">Performance</h2>
+
+            <div className="grid md:grid-cols-5 gap-4 mt-2">
                 <Card>
                     <CardHeader>
                         <CardTitle className="text-md">ROI</CardTitle>
@@ -77,6 +81,77 @@ export function OverviewTab(props: OverviewTabProps) {
                         <span className={`text-2xl font-bold ${theme.text.black}`}>
                             {overview_data.numberOfHoldings}
                         </span>
+                    </CardContent>
+                </Card>
+            </div>
+
+            <h2 className="text-2xl font-bold mt-8">Goals</h2>
+
+            <div className="grid md:grid-cols-5 gap-4 mt-2">
+                <Card>
+                    <CardHeader>
+                        <CardTitle className="text-md">Target</CardTitle>
+                        <CardDescription className="text-sm">Target ROI</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <span className={`text-2xl font-bold ${theme.text.black}`}>
+                            40%
+                        </span>
+                    </CardContent>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <CardTitle className="text-md">Target</CardTitle>
+                        <CardDescription className="text-sm">Target Investment Value</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <span className={`text-2xl font-bold ${theme.text.black}`}>
+                            ₹1,000,000
+                        </span>
+                    </CardContent>
+                </Card>
+                <Card className="md:col-span-2">
+                    <CardHeader>
+                        <CardTitle className="text-md">Status</CardTitle>
+                        <CardDescription className="text-sm">Are you on track?</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <Table>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell>
+                                        PnL Delta
+                                    </TableCell>
+                                    <TableCell className="font-bold">
+                                        100
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>
+                                        Start Month
+                                    </TableCell>
+                                    <TableCell className="font-bold">
+                                        Apr 2023
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>
+                                        End Month
+                                    </TableCell>
+                                    <TableCell className="font-bold">
+                                        Mar 2024
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>
+                                        Current Month
+                                    </TableCell>
+                                    <TableCell className="font-bold">
+                                        July 2024
+                                    </TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
                     </CardContent>
                 </Card>
             </div>
