66cb25d6fb5d18c63a906ebd3a3da5c2400562ff
Yashh Jaggi <yashhjaggi1998@gmail.com>
2024-07-21 19:32:41 -0700

    Miscellaneous changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,21 @@
 import { ThemeProvider, theme, CSSReset } from '@chakra-ui/react';
+import Head from 'next/head';
 import './static/styles/globals.css';
 
 function MyApp({ Component, pageProps }) 
 {
     return (
-        <ThemeProvider theme={theme}>
-            <CSSReset />
-            <Component {...pageProps} />
-        </ThemeProvider>
+        <>
+            <Head>
+                <title>YJ</title>
+                <link rel="icon" href="YJ1.png" />
+                <meta name="description" content="Stan Weinstein Portfolio" />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CSSReset />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
     );
 }
 
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,23 +1,27 @@
 import {useEffect, useState} from "react";
+import Head from 'next/head';
+
 import { useMediaQuery } from "@/hooks/use-media-query";
 
 import { DesktopDrowpdown } from "@/components/custom/DesktopDropdown";
 import { MobileDropdown } from "@/components/custom/MobileDropDown";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
-import theme from "@/utils/theme";
 import { convertINRToUSD } from "@/utils/ConvertINRToUSD";
 import { AnnualOverviewData } from "@/types/AnnualOverviewData";
 import { FinancialYear } from "@/types/FinancialYear";
-import { OverviewTab } from "@/components/custom/OverviewTab";
+import { OverviewTab } from "@/components/custom/tabs/OverviewTab";
+import { ActiveHoldingsTab } from "@/components/custom/tabs/ActiveHoldingsTab";
 
 export default function Portfolio() {
 
     const isDesktop = useMediaQuery("(min-width: 1080px)");
     
-    const [isDrawerOpen, setDrawerOpen] = useState(false); 
+    const [ isDrawerOpen, setDrawerOpen ] = useState(false); 
     const [ selectedFinancialYear, setSelectedFinancialYear ] = useState<FinancialYear | null>(null);
+    const [ activeHoldings, setActiveHoldings ] = useState([]);
+    const [ totalPnl, setTotalPnl ] = useState<number>(0);
+    const [ totalAmountInvested, setTotalAmountInvested ] = useState<number>(0);
 
     const [exchangeRate, setExchangeRate] = useState<number>(0);
     const [annualOverviewData, setAnnualOverviewData] = useState<AnnualOverviewData>({
@@ -44,20 +48,28 @@ export default function Portfolio() {
     }, []);       
 
     useEffect(() => {
-
         async function fetchData() {
 
             if (!selectedFinancialYear) return;
 
             const _FY = selectedFinancialYear?.year;
             const response = await fetch('/api/v1/holdings/annual?financialYear=' + _FY);
+
+            //Unable to fetch live price. Throw an error or use a different approach to handle this
+            if (response.status !== 200) {
+                alert('Unable to fetch data. Please try again later');
+                return;
+            }
             
             const result = await response.json();
             result.absoluteReturnUSD = result.absoluteReturn * exchangeRate;
             result.amountInvestedUSD = result.amountInvested * exchangeRate;
             result.currentInvestmentValueUSD = result.currentInvestmentValue * exchangeRate;
-
             console.log(result);
+
+            setActiveHoldings(result.activeHoldings);
+            setTotalPnl(result.absoluteReturn);
+            setTotalAmountInvested(result.amountInvested);
 
             setAnnualOverviewData(result);
         }
@@ -66,47 +78,51 @@ export default function Portfolio() {
     }, [selectedFinancialYear]);
 
     return (
-        <div className="overflow-hidden border rounded-lg shadow m-8">
-            <div className="border-b p-4">
-                {isDesktop ? 
-                    <DesktopDrowpdown 
-                        selectedFinancialYear={selectedFinancialYear}
-                        setSelectedFinancialYear={setSelectedFinancialYear}
-                        isDrawerOpen={isDrawerOpen}
-                        setDrawerOpen={setDrawerOpen}
-                    /> : 
-                    <MobileDropdown
-                        selectedFinancialYear={selectedFinancialYear}
-                        setSelectedFinancialYear={setSelectedFinancialYear}
-                        isDrawerOpen={isDrawerOpen}
-                        setDrawerOpen={setDrawerOpen}
-                    />
-                }
+        <>
+            <Head>
+                <title>Portfolio</title>
+            </Head>
+        
+            <div className="overflow-hidden border rounded-lg shadow m-8">
+                <div className="border-b p-4">
+                    {isDesktop ? 
+                        <DesktopDrowpdown 
+                            selectedFinancialYear={selectedFinancialYear}
+                            setSelectedFinancialYear={setSelectedFinancialYear}
+                            isDrawerOpen={isDrawerOpen}
+                            setDrawerOpen={setDrawerOpen}
+                        /> : 
+                        <MobileDropdown
+                            selectedFinancialYear={selectedFinancialYear}
+                            setSelectedFinancialYear={setSelectedFinancialYear}
+                            isDrawerOpen={isDrawerOpen}
+                            setDrawerOpen={setDrawerOpen}
+                        />
+                    }
+                </div>
+
+                <div className="p-4">
+                    <Tabs defaultValue="overview">
+                        <TabsList className="grid grid-cols-4 gap-4 w-fit text-sm">
+                            <TabsTrigger value="overview">Overview</TabsTrigger>
+                            <TabsTrigger value="active_holdings">Active</TabsTrigger>
+                            <TabsTrigger value="dividends">Dividends</TabsTrigger>
+                            <TabsTrigger value="tax">Tax</TabsTrigger>
+                        </TabsList>
+
+                        <OverviewTab overview_data={annualOverviewData}/>
+
+                        {selectedFinancialYear && <ActiveHoldingsTab activeHoldings={activeHoldings} selectedFinancialYear={selectedFinancialYear} />}
+                        <TabsContent value="dividends">
+                            <div>Dividends</div>
+                        </TabsContent>
+                        <TabsContent value="tax">
+                            <div>Tax</div>
+                        </TabsContent>
+                    </Tabs>
+
+                </div>
             </div>
-
-            <div className="p-4">
-                <Tabs defaultValue="overview">
-                    <TabsList className="grid grid-cols-4 gap-4 w-fit">
-                        <TabsTrigger value="overview">Overview</TabsTrigger>
-                        <TabsTrigger value="holdings">Holdings</TabsTrigger>
-                        <TabsTrigger value="dividends">Dividends</TabsTrigger>
-                        <TabsTrigger value="tax">Tax</TabsTrigger>
-                    </TabsList>
-
-                    <OverviewTab overview_data={annualOverviewData}/>
-
-                    <TabsContent value="holdings">
-                        <div>Holdings</div>
-                    </TabsContent>
-                    <TabsContent value="dividends">
-                        <div>Dividends</div>
-                    </TabsContent>
-                    <TabsContent value="tax">
-                        <div>Tax</div>
-                    </TabsContent>
-                </Tabs>
-
-            </div>
-        </div>
+        </>
     );
 };
