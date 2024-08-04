4d5d4c6e45fac3b349976b937515c71a730ca0f7
yashhjaggi <135260974+yashhjaggi@users.noreply.github.com>
2024-08-04 16:20:05 -0700

    Creating the goal constant

--- types/Goals.ts
+++ types/Goals.ts
@@ -1,5 +1,19 @@
-export type Goals = {
+export type Goal = {
     targetRoi: number;
     targetPnL: number;
     targetInvestmentValue: number;
+}
+
+// JSON object to store goals for each financial year
+export const goals: { [key: string]: Goal  } = {
+    "2023": {
+        targetRoi: 40,
+        targetPnL: 400,
+        targetInvestmentValue: 1000,
+    },
+    "2024": {
+        targetRoi: 40,
+        targetPnL: 5000,
+        targetInvestmentValue: 12500,
+    },
 }
