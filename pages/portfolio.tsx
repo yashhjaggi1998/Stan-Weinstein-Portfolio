9a92ca0e323f9c9127edb5ae2fb8b82739be631b
Yashh Jaggi <yashhjaggi1998@gmail.com>
2024-09-11 00:23:39 -0700

    FIX: active tab for loading spinner

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -127,7 +127,7 @@ export default function Portfolio() {
                             />
                         }
 
-                        {selectedFinancialYear && (isDesktop ?
+                        {selectedFinancialYear && !isDataLoading && (isDesktop ?
                             <ActiveHoldingsTab
                                 activeHoldings={activeHoldings}
                                 selectedFinancialYear={selectedFinancialYear}
